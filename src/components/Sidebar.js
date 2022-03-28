cfcc81fe0fd3e99fcf162a84dd5159c3f1e1aa33
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-28 07:41:24 +0100

    sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,35 @@
-// styles
-import './Sidebar.css'
+import { NavLink } from "react-router-dom"
 
-import React from 'react'
+// styles & images
+import "./Sidebar.css"
+import DashboardIcon from '../assets/dashboard_icon.svg'
+import AddIcon from '../assets/add_icon.svg'
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <div className="user">
+          {/* avatar & username here later */}
+          <p>Hey user</p>  
+        </div>  
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink exact to="/">
+                <img src={DashboardIcon} alt="dashboard icon" />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/create">
+                <img src={AddIcon} alt="add project icon" />
+                <span>New Project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   )
 }
