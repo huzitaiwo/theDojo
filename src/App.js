e0aa035eeac499c46e351953f8becec0a694aeab
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-28 07:19:50 +0100

    imported pages components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+// styles and components
 import './App.css'
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
 function App() {
   return (
