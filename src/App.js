16208433f78c6d8e3267fed6504a14504acf313a
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-28 07:22:21 +0100

    setup ROute

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Dashboard from './pages/dashboard/Dashboard'
 import Create from './pages/create/Create'
+import Project from './pages/project/Project'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 
 function App() {
   return (
     <div className="App">
-      app
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Dashboard />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+          <Route path='/project:id'>
+            <Project />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
