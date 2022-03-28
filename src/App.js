99710522b9122e0cac9079de17f52db9af77c0ff
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-28 07:18:11 +0100

    imported components from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,2 +1,10 @@
 // styles
 import './Login.css'
+
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>Login</div>
+  )
+}
