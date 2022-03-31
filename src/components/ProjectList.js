bc044baed4154eb86aeb3ea9027ab17be8fc9118
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-31 09:32:56 +0100

    styled projectList component

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,13 +1,26 @@
-// styles
+import { Link } from 'react-router-dom'
+
+// styles and components
 import './ProjectList.css'
+import Avatar from './Avatar'
 
 export default function ProjectList({ projects }) {
     
   return (
-    <div>
+    <div className='project-list'>
       {projects.length === 0 && <p>No project yet!!!</p>}
       {projects.map(project => (
-        <p key={project.id}>{project.name}</p>
+        <Link to={`/projects/${project.id}`} key={project.id}>{project.name}
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <ul className='assigned-to'>
+            {project.assignedUsersList.map(user=> (
+              <li key={user.photoURL}>
+                <Avatar src={user.photoURL}/>
+              </li>          
+            ))}
+          </ul>
+        </Link>
       ))}
     </div>
   )
