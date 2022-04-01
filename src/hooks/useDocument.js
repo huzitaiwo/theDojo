ffb66626e6fccbc178ca3069d458681e112a6d49
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-01 08:16:23 +0100

    creating useDocument hook

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,5 +1,18 @@
 import { useEffect, useState } from 'react'
+import { projectFirestore } from '../firebase/config'
 
-export const useDocument = (collection, id) => {
+export const useDocument = async (collection, id) => {
+  const [document, setDocument] = useState(null)
+  const [error, setError] = useState(null)
+
+  // realtime data for document
+  useEffect(() => {
+    const ref = await projectFirestore.collection(collection).doc(id)
+
+    ref.onSnapshot(snapshot => {
+      setDocument({ ...snapshot.data(), id: snapshot.id })
+      setError(null)
+    })
+  }, [collection, id])
 
 } 
