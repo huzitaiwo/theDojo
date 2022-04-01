a73444518e874205da1e5842d548cc29b5f42e1d
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-01 08:45:21 +0100

    created project summary component

---
+++ src/pages/project/projectComment.js
@@ -0,0 +1,9 @@
+import { useState } from "react"
+import { timestamp } from "../../firebase/config"
+import { useAuthContext } from "../../hooks/useAuthContext"
+
+export default function projectComment() {
+  return (
+    <div>projectComment</div>
+  )
+}
