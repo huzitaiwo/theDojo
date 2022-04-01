2c5ad2715de14ca2bd50b9e67b8dc621ea305781
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-01 08:51:09 +0100

    added new comment form

--- src/pages/project/projectComment.js
+++ src/pages/project/projectComment.js
@@ -3,7 +3,29 @@ import { timestamp } from "../../firebase/config"
 import { useAuthContext } from "../../hooks/useAuthContext"
 
 export default function projectComment() {
+  const [newComment, setNewComment ] = useState('')
+  const { user } = useAuthContext()
+
+  const handleSubmit =  async (e) => {
+    e.preventDefault()
+
+    // const comm
+  }
+
   return (
-    <div>projectComment</div>
+    <div onSubmit={handleSubmit} className="project-comments">
+      <h4>Project comments</h4>
+      <form className="add-comment">
+        <label>
+          <span>Add new comment</span>
+          <textarea
+            required
+            onChange={e => {setNewComment(e.target.value)}}
+            value={newComment}          
+          ></textarea>
+          <button className="btn">Add comment</button>
+        </label>
+      </form>
+    </div>
   )
 }
