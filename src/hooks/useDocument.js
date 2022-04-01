571aa6568cfc76af668517b04b560cfe026131f1
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-01 08:30:05 +0100

    fixed known id bug

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react'
 import { projectFirestore } from '../firebase/config'
 
-export const useDocument = async (collection, id) => {
+export const useDocument = (collection, id) => {
   const [document, setDocument] = useState(null)
   const [error, setError] = useState(null)
 
   // realtime data for document
   useEffect(() => {
-    const ref = await projectFirestore.collection(collection).doc(id)
+    const ref = projectFirestore.collection(collection).doc(id)
 
     const unsubscribe = ref.onSnapshot(snapshot => {
-      setDocument({ ...snapshot.data(), id: snapshot.id })
-      setError(null)
+      if (snapshot.data()) {
+        setDocument({ ...snapshot.data(), id: snapshot.id })
+        setError(null)
+      }
+      else {
+        setError('no such document')
+      }
+      console.log(snapshot)
     }, (err) => {
       console.log(err.message)
       setError('failed to get document')
