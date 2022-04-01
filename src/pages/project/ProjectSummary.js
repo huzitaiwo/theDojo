2bc11be0131f7fae08bb95e3aee8355a96ead0bf
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-01 08:41:25 +0100

    styled project summary

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -8,11 +8,13 @@ export default function ProjectSummary({ project }) {
         <p className="due-date">Project due by {project.dueDate.toDate().toDateString()}</p>
         <p className="details">{project.details}</p>
         <h4>Project is assigned to:</h4>
-        {project.assignedUsersList.map(user => (
-          <div key={user.id}>
-            <Avatar src={user.photoURL} />
-          </div>
-        ))}
+        <div className="assigned-users">
+          {project.assignedUsersList.map(user => (
+            <div key={user.id}>
+              <Avatar src={user.photoURL} />
+            </div>
+          ))}
+        </div>
       </div> 
     </div>
   )
