45a27170aece9c97c3a40f4cbc309d719fc16134
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-01 08:24:19 +0100

    rendering projectDetails

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,10 +1,20 @@
+import { useParams } from 'react-router-dom'
+import { useDocument } from '../../hooks/useDocument'
+
 // styles
 import './Project.css'
 
-import React from 'react'
-
 export default function Project() {
+  const { id } = useParams()
+  const { error, document } = useDocument('projects', id)
+
+  if(error) {
+    return <div className='error'>{eror}</div>
+  }
+
   return (
-    <div>Project</div>
+    <div>
+
+    </div>
   )
 }
