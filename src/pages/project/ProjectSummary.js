f3c8e3248ebff4907504d5ae425f8ef8da9706df
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-02 10:09:16 +0100

    created handleDelete function mark as complete

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,10 +1,12 @@
 import { useFirestore } from "../../hooks/useFirestore"
+import { useAuthContext } from "../../hooks/useAuthContext"
 
 //components
 import Avatar from "../../components/Avatar"
 
 export default function ProjectSummary({ project }) {
   const { deleteDocument } = useFirestore('projects')
+  const { user } = useAuthContext()
 
   const handleDelete = e => {
     deleteDocument(project.id)
@@ -14,6 +16,7 @@ export default function ProjectSummary({ project }) {
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className="due-date">Project due by {project.dueDate.toDate().toDateString()}</p>
         <p className="details">{project.details}</p>
         <h4>Project is assigned to:</h4>
@@ -24,7 +27,9 @@ export default function ProjectSummary({ project }) {
             </div>
           ))}
         </div>
-        <button onClick={handleDelete} className="btn">Mark as complete</button>
+        {user.uid === project.createdBy.id && (
+          <button onClick={handleDelete} className="btn">Mark as complete</button>
+        )}
       </div> 
     </div>
   )
