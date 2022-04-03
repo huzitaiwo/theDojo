77f30f6d22e9229e18d3f2eeafb72a663cfb3cfd
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-03 21:30:24 +0100

    creating newFilter function

--- src/pages/dashboard/Filter.js
+++ src/pages/dashboard/Filter.js
@@ -1,10 +1,26 @@
+import { useState } from "react"
+
 const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
 
 export default function Filter() {
+  const [currentFilter, setCurentFilter] = useState('all')
+
+  const handleClick = (newFilter) => {
+    console.log(newFilter)
+    setCurentFilter(newFilter)
+  }
+
   return (
     <div className='project-filter'>
       <nav>
-        
+        {filterList.map(filter => (
+          <button 
+            key={filter}
+            onClick={() => handleClick(filter)}
+          >
+            {f}
+          </button>  
+        ))}
       </nav>
     </div>
   )
