bbb4d62509ad923fbb58a73eddf9a0311cb7b29a
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-03 21:26:19 +0100

    created filter component

---
+++ src/pages/dashboard/Filter.js
@@ -0,0 +1,11 @@
+const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
+
+export default function Filter() {
+  return (
+    <div className='project-filter'>
+      <nav>
+        
+      </nav>
+    </div>
+  )
+}
